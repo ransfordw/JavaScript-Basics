6b83c5c48aa9ca133b84ca2d6bea976e9626dd58
Ransford Walker <ransfordw@gmail.com>
2019-05-30 16:37:44 -0400

    Finished first function lesson, some folder renaming

---
+++ basics/functions/index.js
@@ -0,0 +1,11 @@
+// function keyword declares the function
+// In this case 'greet' is the name of the function
+
+function greet(name, lastName) { // Inside the curly boys is the body of the function
+    console.log(`Hello ${name} ${lastName}`);
+}
+
+greet('Ransford', 'Walker');
+greet('Kenn', 'Pascasio');
+greet('Tom', 'McClellan');
+
