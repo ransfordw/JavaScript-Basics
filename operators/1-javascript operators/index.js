0ae5895b6d34339a3d2a112417be78ce3a23248a
Ransford Walker <ransfordw@gmail.com>
2019-05-31 12:31:41 -0400

    basic operator lessons completed

---
+++ operators/1-javascript operators/index.js
@@ -0,0 +1,17 @@
+let x = 10;
+let y = 3;
+
+console.log(x + y); // + addition operator
+console.log(x - y); // - subtraction operator
+console.log(x * y); // * multiplication operator
+console.log(x / y); // / division operator
+console.log(x % y); // % (modulus)remainder of division operator
+console.log(x ** y); // ** exponentiation operator (x to the power of y or x^y)
+
+// Increment Operator ++
+console.log(++x); // the value of x will increase by one and then we will see the value of x
+console.log(y++); // the value of y will be displayed and then increased by 1
+
+// Decrement Operator --
+console.log(x--);
+console.log(--x); // Same as above but decreaseing by one
