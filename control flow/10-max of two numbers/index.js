74a41586be679f2b878686e9c25a06abe829be29
Ransford Walker <ransfordw@gmail.com>
2019-06-03 13:18:05 -0400

    completed exercise 10 in control flow

---
+++ control flow/10-max of two numbers/index.js
@@ -0,0 +1,11 @@
+
+let number = getMaxOfTwoNumbers(1, 2);
+console.log(number);
+
+function getMaxOfTwoNumbers(x, y) {
+    // if (x > y)
+    //     return x;
+    // return y;
+
+    return (x > y) ? x : y;
+}
