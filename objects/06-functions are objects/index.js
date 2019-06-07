f8577d5300fcef42e8d653fd4ddc61cf4e626ed2
Ransford Walker <ransfordw@gmail.com>
2019-06-07 10:23:41 -0400

    finished objects lesson 6

---
+++ objects/06-functions are objects/index.js
@@ -0,0 +1,12 @@
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function () {
+        console.log('draw');
+    }
+}
+
+Circle.call({}, 1,2,3);
+Circle.apply({}, [1,2,3]);
+
+
+const circle = new Circle(1);
