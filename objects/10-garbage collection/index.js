3880e4a7b10837667fa709bccd15a2c68f52d6a8
Ransford Walker <ransfordw@gmail.com>
2019-06-10 09:36:10 -0400

    Garbage collection lesson completed.

---
+++ objects/10-garbage collection/index.js
@@ -0,0 +1,7 @@
+
+let circle = {}; // at the time of initialization, memory is automatically allocated
+console.log(circle); // we do not need deallocate memory after we are finished with the object, variable, etc.
+
+// Garbage Collector
+// Finds unused variables and constants and deallocates the memory for you
+
