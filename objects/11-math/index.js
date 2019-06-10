e3a8fff3c4d6c186262878d5da870d13bae76003
Ransford Walker <ransfordw@gmail.com>
2019-06-10 15:28:02 -0400

    Math lesson: complete

---
+++ objects/11-math/index.js
@@ -0,0 +1,9 @@
+
+// 'Math' is a built in object with properties and methods for mathematical equations
+
+console.log('Math.random()',Math.random()); // returns a random number
+
+console.log('Math.round(1.7)', Math.round(1.7));
+
+console.log('Math.max(1,2,3,4,5)', Math.max(1,2,3,4,5));
+console.log('Math.min(1,2,3,4,5)', Math.min(1,2,3,4,5));
