58a2fc9f62073046af072693cb8c4cb0afe475e2
Ransford Walker <ransfordw@gmail.com>
2019-06-12 09:12:04 -0400

    Template literal lesson completed

---
+++ objects/13-template literals/index.js
@@ -0,0 +1,32 @@
+
+// String primitive
+const message = 'This is my\n first message';
+
+const messageTwo = 'This is my\n' +
+'first message';
+
+// Object {}
+// Boolean true, false
+// String '',""
+// Template ``
+
+const anotherMessage = 
+`This is my
+'first' message`;
+
+// Instead of 
+// const name = 'John';
+// const message = 'Hi ' + name + ',\n';
+
+
+const name = 'Mark';
+
+const emailMessage = 
+`Oh hi ${name},
+
+Thank you for joining my mailing list.
+
+Regards,
+Ransford`;
+
+console.log(emailMessage);
