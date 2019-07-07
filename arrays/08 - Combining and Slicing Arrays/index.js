faed99c956ca4588cfc26bac3d9dbc33bbd17e83
Ransford Walker <ransfordw@gmail.com>
2019-07-07 17:32:53 -0400

    setup spread operator

--- arrays/08 - Combining and Slicing Arrays/index.js
+++ arrays/08 - Combining and Slicing Arrays/index.js
@@ -10,3 +10,6 @@ const slice = combined.slice(2, 4); // starts at index 2 and ends at 4
 // const slice = combined.slice(); // copies the original array
 
 console.log(slice);
+
+// when using concat with reference types, it copies the reference, not the actual object.
+// so if a reference type is altered, it is altered everywhere.
---
+++ arrays/09 - The Spread Operator/index.js
@@ -0,0 +1,7 @@
+
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+const combined = first.concat(second);
+
+const slice = combined.slice();
