15b768f93f101633d0439a5bf71d585b8eb54d28
Ransford Walker <ransfordw@gmail.com>
2019-07-07 17:36:19 -0400

    finished spread operator

--- arrays/09 - The Spread Operator/index.js
+++ arrays/09 - The Spread Operator/index.js
@@ -2,6 +2,9 @@
 const first = [1, 2, 3];
 const second = [4, 5, 6];
 
-const combined = first.concat(second);
+const combined = [...first, 'a', ...second, 'b'];
+// when we spread an array, all of its elements 
+// are returned individually
 
-const slice = combined.slice();
+const copy = [...combined]; // returns all the elements of the combined array into this new array.
+
