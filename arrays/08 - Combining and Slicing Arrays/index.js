094a5ba1e46bd6a91d4264632f9255e3c4aaa0b5
Ransford Walker <ransfordw@gmail.com>
2019-07-07 17:19:23 -0400

    completed the cobining and slicing arrays lecture

---
+++ arrays/08 - Combining and Slicing Arrays/index.js
@@ -0,0 +1,12 @@
+
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+const combined = first.concat(second);
+console.log(combined);
+
+const slice = combined.slice(2, 4); // starts at index 2 and ends at 4
+// const slice = combined.slice(2); // Starts at 2 and gets the rest 
+// const slice = combined.slice(); // copies the original array
+
+console.log(slice);
