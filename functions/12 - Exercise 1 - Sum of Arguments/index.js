00f32ce47525a4d0d1bd013543c603830c0a7cb5
Ransford Walker <ransfordw@gmail.com>
2019-07-08 12:07:29 -0400

    added mosh's solution

--- functions/12 - Exercise 1 - Sum of Arguments/index.js
+++ functions/12 - Exercise 1 - Sum of Arguments/index.js
@@ -25,3 +25,13 @@ try {
 catch (e) {
     alert(e);
 }
+
+function moshSum(...items){
+    if(items.length === 1 && Array.isArray(items[0]))
+    items = [...items[0]];
+
+    return items.reduce((a,b) => a+b);
+}
+
+console.log(moshSum([1,2,3,4,5]));
+console.log(moshSum(1,2,3,4,5));
